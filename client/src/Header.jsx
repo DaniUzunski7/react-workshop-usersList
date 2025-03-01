9ba11b264f7e622543cf8ede43fc192328236c52
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-01 14:48:43 +0200

    Add client resources
    Header and Footer component

---
+++ client/src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header class="header">
+        <div class="logo">
+          <span class="course">React Workshop - UsersList</span>
+        </div>
+      </header>
+    )
+}
