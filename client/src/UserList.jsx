4ad8de12bcaa8e6b9e47f910bf2c9d14b16327a3
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-09 13:59:41 +0200

    Add loading shade
    Extract LoadingShade component

--- client/src/UserList.jsx
+++ client/src/UserList.jsx
@@ -7,30 +7,42 @@ import { UserCreate } from "./userCreate";
 import {UserDetails} from "./UserDetails"
 import { UserDelete } from "./UserDelete";
 import { NoContent } from "./NoContent";
+import { LoadingShade } from "./LoadingShade";
 
 export function UserList() {
   const [users, setUsers] = useState([]);
+ 
   const [showCreate, setShowCreate] = useState(false);
   const [showDetails, setShowDetails] = useState(false);
   const [showDelete, setShowDelete] = useState(false);
+  
   const [userDetails, setUserDetails] = useState(null);
   const [userIdEdit, setUserIdEdit] = useState(null);
   const [noUsers, setNoUsers] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    const userData = UserServices.getAll().then((result) => {
-      setUsers(result);
-    });
+    const userData = UserServices.getAll()
+        .then((result) => {
+            setUsers(result)
+            setIsLoading(false)
+        })
+        .catch( (error) => {
+            console.log('Error fetching users:', error);
+            setIsLoading(false);
+        })
   }, []);
 
   useEffect( () => {
 
     if(users.length < 1){
         setNoUsers(true)
-    } else if (users.length >= 1){
+    } 
+    
+    if (users.length >= 1){
         setNoUsers(false);
     }
-    console.log(noUsers);
     
   }, [users])
   
@@ -114,6 +126,8 @@ export function UserList() {
     <section className="card users-container">
       <Search />
 
+      {isLoading && <LoadingShade/>}
+
       {showCreate && 
         <UserCreate 
             onClose={addUserCloseHandler} 
@@ -145,36 +159,7 @@ export function UserList() {
         
       <div className="table-wrapper">
         <div>
-          {/* <!-- Overlap components  --> */}
-
-          {/* <!-- <div className="loading-shade"> --> */}
-          {/* <!-- Loading spinner  --> */}
-          {/* <!-- <div className="spinner"></div> --> */}
-          {/* <!--  */}
-          {/* No users added yet  --> */}
-
-          {/* <!-- <div className="table-overlap"> */}
-          {/* <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="triangle-exclamation"
-                className="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
-                ></path>
-              </svg>
-              <h2>There is no users yet.</h2> */}
-          {/* </div> --> */}
-
-         
         </div>
-
         {noUsers && <NoContent/>}
         <table className="table">
           <thead>
